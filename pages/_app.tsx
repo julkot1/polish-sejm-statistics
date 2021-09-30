a9926f07480f0b1327e093df86ae1315b176c379
julkot1 <julian.kotlowski@gmail.com>
2021-09-30 15:59:47 +0200

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+ body{
+   margin: 0;
+ }
+`
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 export default MyApp
